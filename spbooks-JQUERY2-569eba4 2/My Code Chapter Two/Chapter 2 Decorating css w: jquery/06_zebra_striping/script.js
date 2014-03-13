76bcb73490604c89c40fdd779c00c127ed8fdc3e
Jessica Waite <imjessw@gmail.com>
2014-03-13 14:59:13 -0400

    notes finished for 2/06
    Added notes on why this takes place after doc ready

--- spbooks-JQUERY2-569eba4 2/My Code Chapter Two/Chapter 2 Decorating css w: jquery/06_zebra_striping/script.js
+++ spbooks-JQUERY2-569eba4 2/My Code Chapter Two/Chapter 2 Decorating css w: jquery/06_zebra_striping/script.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
 	// broken down : The code is saying on every even table row after 
 	// the tbody change the css to have a background color of purple
  	$('#celebs tbody tr:even').css('background-color','#331524');
+ 		// this funtion has to be run inside doc ready bc if it wasn't, it would
+ 		// run before thr page loded leading to no tr to actuallt affect.
+ 		
+
+
 });
