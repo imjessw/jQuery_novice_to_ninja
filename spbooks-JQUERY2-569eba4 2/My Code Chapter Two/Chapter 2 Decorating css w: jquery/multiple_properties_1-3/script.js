a2197211b9db81ef021735c31477e8f491f4e537
Jessica Waite <imjessw@gmail.com>
2014-03-18 16:55:17 -0400

    .css vs .addClass
    I am explaining why you would use .addClass over .css to add styles

--- spbooks-JQUERY2-569eba4 2/My Code Chapter Two/Chapter 2 Decorating css w: jquery/multiple_properties_1-3/script.js
+++ spbooks-JQUERY2-569eba4 2/My Code Chapter Two/Chapter 2 Decorating css w: jquery/multiple_properties_1-3/script.js
@@ -16,9 +16,32 @@ $(document).ready(function(){
 	// This is particularly useful when needing to specify multiple 
 	// key/value pairs (large number of properties)
 	// Example Below:
-		$('#celebs tbody tr:even').css(
-			{'background-color':'#331524',
-			 'color':'#ff69b4',
-			 'font-size': '11px'
-		});
-});
+			// $('#celebs tbody tr:even').css(
+			// 	{'background-color':'#331524',
+			// 	 'color':'#ff69b4',
+			// 	 'font-size': '11px'
+			// });
+	// Now the above code doe steh job however the problem is this:
+	// this adds the style sin the HTML, and no one likes that... its gross 
+	// so what do we do? we create a custom class in our css to call the 
+	// class name when we need it to invoke the new styles. This keeps everything pretty
+	// and as I put it keeps it looking like "synatactic sugar"
+	// First we go into our css file and add and in jQuery add an . add class
+	// Basic set up:
+		// ('div').addClass('class_name');
+	// Also if you want to add multiple new classes
+		// ('div').addClass('class_name1 class_name2 class_name3');
+	// Now how to apply this to our website
+	 		// **In CSS**
+	 			// .zebra{
+	 				// 'background-color':'#331524',
+					//'color':'#ff69b4',
+					//'font-size': '11px'
+	 			//}
+	// Now back in jQuery using the previous way of targeting what we want to highlight 
+	// we are now going to use an .addClass of .zebra which is adding the zebra class to 
+	// every even tr
+		// $('#celebs tr:even').addClass('zebra');
+	// this gives us the same results as using the .css however it removes the inline styles. 
+
+// });
